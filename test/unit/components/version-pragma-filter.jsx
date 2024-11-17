26811f786d9b7bf046d9f837177c91dd535516bc
Manish Jha <stayconnectedwithmanish@gmail.com>
2024-11-17 09:21:05 +0530

    version pragma filter

--- test/unit/components/version-pragma-filter.jsx
+++ test/unit/components/version-pragma-filter.jsx
@@ -1,67 +1,74 @@
 import React from "react"
-import { shallow } from "enzyme"
+import { render, screen } from "@testing-library/react"
+import '@testing-library/jest-dom'  // Add this import
 import VersionPragmaFilter from "core/components/version-pragma-filter"
 
-describe("<VersionPragmaFilter/>", function(){
-  it("renders children for a Swagger 2 definition", function(){
+describe("<VersionPragmaFilter/>", () => {
+  it("renders children for a Swagger 2 definition", () => {
     // When
-    let wrapper = shallow(
+    render(
       <VersionPragmaFilter isSwagger2={true} isOAS3={false}>
         hello!
       </VersionPragmaFilter>
     )
 
     // Then
-    expect(wrapper.find("div").length).toEqual(1)
-    expect(wrapper.find("div").text()).toEqual("hello!")
+    expect(screen.getByText("hello!")).toBeInTheDocument()
   })
-  it("renders children for an OpenAPI 3 definition", function(){
+
+  it("renders children for an OpenAPI 3 definition", () => {
     // When
-    let wrapper = shallow(
+    render(
       <VersionPragmaFilter isSwagger2={false} isOAS3={true}>
         hello!
       </VersionPragmaFilter>
     )
 
     // Then
-    expect(wrapper.find("div").length).toEqual(1)
-    expect(wrapper.find("div").text()).toEqual("hello!")
+    expect(screen.getByText("hello!")).toBeInTheDocument()
   })
-  it("renders children when a bypass prop is set", function(){
+
+  it("renders children when a bypass prop is set", () => {
     // When
-    let wrapper = shallow(
+    render(
       <VersionPragmaFilter bypass>
         hello!
       </VersionPragmaFilter>
     )
 
     // Then
-    expect(wrapper.find("div").length).toEqual(1)
-    expect(wrapper.find("div").text()).toEqual("hello!")
+    expect(screen.getByText("hello!")).toBeInTheDocument()
   })
-  it("renders the correct message for an ambiguous-version definition", function(){
+
+  it("renders the correct message for an ambiguous-version definition", () => {
     // When
-    let wrapper = shallow(
+    render(
       <VersionPragmaFilter isSwagger2={true} isOAS3={true}>
         hello!
       </VersionPragmaFilter>
     )
 
     // Then
-    expect(wrapper.find("div.version-pragma__message--ambiguous").length).toEqual(1)
-    expect(wrapper.find("div.version-pragma__message--missing").length).toEqual(0)
+    const { container } = render(
+      <VersionPragmaFilter isSwagger2={true} isOAS3={true}>
+        hello!
+      </VersionPragmaFilter>
+    )
+    
+    expect(container.querySelector(".version-pragma__message--ambiguous")).toBeInTheDocument()
+    expect(container.querySelector(".version-pragma__message--missing")).not.toBeInTheDocument()
   })
-  it("renders the correct message for a missing-version definition", function(){
+
+  it("renders the correct message for a missing-version definition", () => {
     // When
-    let wrapper = shallow(
+    const { container } = render(
       <VersionPragmaFilter isSwagger2={false} isOAS3={false}>
         hello!
       </VersionPragmaFilter>
     )
 
     // Then
-    expect(wrapper.find("div.version-pragma__message--missing").length).toEqual(1)
-    expect(wrapper.find("div.version-pragma__message--ambiguous").length).toEqual(0)
+    expect(container.querySelector(".version-pragma__message--missing")).toBeInTheDocument()
+    expect(container.querySelector(".version-pragma__message--ambiguous")).not.toBeInTheDocument()
   })
-
 })
